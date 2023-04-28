463fe184e5903a9b9c351f537767e625f75f8719
Michal13945 <115593567+Michal13945@users.noreply.github.com>
2023-04-28 17:55:23 +0200

    Operation conversion and operator overloading

--- Pudelko.cs
+++ Pudelko.cs
@@ -4,7 +4,7 @@ using System.Globalization;
 
 namespace Projekt_Pudelko
 {
-    public sealed class Pudelko : IFormattable
+    public sealed class Pudelko : IFormattable, IEquatable<Pudelko>
     {
         private readonly double a;
         private readonly double b;
@@ -14,7 +14,7 @@ namespace Projekt_Pudelko
 
         public double A
         {
-            get { return Math.Round(a,3); }
+            get { return Math.Round(a, 3); }
         }
 
         public double B
@@ -24,6 +24,14 @@ namespace Projekt_Pudelko
         public double C
         {
             get { return Math.Round(c, 3); }
+        }
+        public double Objetosc
+        {
+            get { return Math.Round(GetNumberInUnit(a, "m") * GetNumberInUnit(b, "m") * GetNumberInUnit(c, "m"), 9); }
+        }
+        public double Pole
+        {
+            get { return Math.Round(2 * GetNumberInUnit(a, "m") * GetNumberInUnit(b, "m") + 2 * GetNumberInUnit(a, "m") * GetNumberInUnit(c, "m") + 2 * GetNumberInUnit(b, "m") * GetNumberInUnit(c, "m"), 6); }
         }
         public Pudelko()
         {
@@ -53,7 +61,68 @@ namespace Projekt_Pudelko
 
         }
 
-        private double GetNumberInUnit(double number, string unitAsString)
+        public static explicit operator double[] (Pudelko p)
+        {
+            double[] result = new double[3];
+            result[0] = GetNumberInUnitStatic(p.a, p.unitOfMeasure, "m");
+            result[1] = GetNumberInUnitStatic(p.b, p.unitOfMeasure, "m");
+            result[2] = GetNumberInUnitStatic(p.c, p.unitOfMeasure, "m");
+            return result;
+        }
+
+        public static implicit operator Pudelko((double a, double b, double c) result)
+        {
+            return new Pudelko(result.a, result.b, result.c, UnitOfMeasure.milimeter);
+        }
+        private double  GetNumberInUnit(double number, string unitAsString)
+        {
+            UnitOfMeasure unit = UnitOfMeasure.meter;
+            switch (unitAsString)
+            {
+                case "mm":
+                    unit = UnitOfMeasure.milimeter;
+                    break;
+                case "cm":
+                    unit = UnitOfMeasure.centimeter;
+                    break;
+                case "m":
+                    unit = UnitOfMeasure.meter;
+                    break;
+            }
+
+            if(unit == unitOfMeasure)
+            {
+                return number;
+            }
+            else if(unit == UnitOfMeasure.meter && unitOfMeasure == UnitOfMeasure.centimeter)
+            {
+                return number / 100;
+            }
+            else if(unit == UnitOfMeasure.centimeter && unitOfMeasure == UnitOfMeasure.milimeter)
+            {
+                return  number / 10;
+            }
+            else if (unit == UnitOfMeasure.centimeter && unitOfMeasure == UnitOfMeasure.meter)
+            {
+                return number * 100;
+            }
+            else if (unit == UnitOfMeasure.meter && unitOfMeasure == UnitOfMeasure.milimeter)
+            {
+                return number * 1000;
+            }
+            else if (unit == UnitOfMeasure.milimeter && unitOfMeasure == UnitOfMeasure.centimeter)
+            {
+                return number * 10;
+            }
+            else if (unit == UnitOfMeasure.milimeter && unitOfMeasure == UnitOfMeasure.meter)
+            {
+                return number * 1000;
+            }
+
+            return number;
+        }
+
+         private static double  GetNumberInUnitStatic(double number, UnitOfMeasure unitOfMeasure, string unitAsString)
         {
             UnitOfMeasure unit = UnitOfMeasure.meter;
             switch (unitAsString)
@@ -130,6 +199,70 @@ namespace Projekt_Pudelko
             {
                 return $"«{string.Format("{0:#,0.000}", Math.Round(GetNumberInUnit(A, format), 3))}» «{format}» {'\u00d7'} «{string.Format("{0:#,0.000}", Math.Round(GetNumberInUnit(B, format), 3))}» «{format}» {'\u00d7'} «{string.Format("{0:#,0.000}", Math.Round(GetNumberInUnit(C, format), 3))}» «{format}»\r\n\r\n";
             }
+        }
+
+        public bool Equals(Pudelko? other)
+        {
+            return GetNumberInUnit(a, "m") == GetNumberInUnit(other.a,"m") &&
+                   GetNumberInUnit(b, "m") == GetNumberInUnit(other.b, "m") &&
+                   GetNumberInUnit(c, "m") == GetNumberInUnit(other.c, "m");
+        }
+
+        public static bool operator==(Pudelko p1, Pudelko p2)
+        {
+            return GetNumberInUnitStatic(p1.a, p1.unitOfMeasure, "m") == GetNumberInUnitStatic(p2.a, p2.unitOfMeasure, "m") &&
+                  GetNumberInUnitStatic(p1.b, p1.unitOfMeasure, "m") == GetNumberInUnitStatic(p2.b, p2.unitOfMeasure, "m") &&
+                  GetNumberInUnitStatic(p1.c, p1.unitOfMeasure, "m") == GetNumberInUnitStatic(p2.c, p2.unitOfMeasure, "m");
+        }
+
+        public static bool operator!=(Pudelko p1, Pudelko p2)
+        {
+            return GetNumberInUnitStatic(p1.a, p1.unitOfMeasure, "m") != GetNumberInUnitStatic(p2.a, p2.unitOfMeasure, "m") ||
+                  GetNumberInUnitStatic(p1.b, p1.unitOfMeasure, "m") != GetNumberInUnitStatic(p2.b, p2.unitOfMeasure, "m") ||
+                  GetNumberInUnitStatic(p1.c, p1.unitOfMeasure, "m") != GetNumberInUnitStatic(p2.c, p2.unitOfMeasure, "m");
+        }
+
+        public static Pudelko operator+(Pudelko p1, Pudelko p2)
+        {
+            double a = 0;
+            double b = 0;
+            double c = 0;
+
+            double p1A = GetNumberInUnitStatic(p1.a, p1.unitOfMeasure, "m");
+            double p1B = GetNumberInUnitStatic(p1.b, p1.unitOfMeasure, "m");
+            double p1C = GetNumberInUnitStatic(p1.c, p1.unitOfMeasure, "m");
+            double p2A = GetNumberInUnitStatic(p2.a, p2.unitOfMeasure, "m");
+            double p2B = GetNumberInUnitStatic(p2.b, p2.unitOfMeasure, "m");
+            double p2C = GetNumberInUnitStatic(p2.c, p2.unitOfMeasure, "m");
+
+            a = p1A + p2A;
+            b = p1B + p2B;
+            
+            if(p2C >= p1C)
+            {
+                c = p2C;
+            }
+            else
+            {
+                c = p1C;
+            }
+
+
+            return new Pudelko(a, b, c);
+        }
+
+        public override int GetHashCode()
+        {
+            double[] dimensions = new double[] { this.a, this.b, this.c };
+            Array.Sort(dimensions);
+            int hash = 17;
+
+            foreach (double dimension in dimensions)
+            {
+                hash = hash * 31 + dimension.GetHashCode();
+            }
+
+            return hash;
         }
     }
 }
