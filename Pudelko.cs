b73da2ac69762af3d688ade7b81e9a984e8906e2
Michal13945 <115593567+Michal13945@users.noreply.github.com>
2023-04-28 18:00:32 +0200

    Indexer added

--- Pudelko.cs
+++ Pudelko.cs
@@ -74,6 +74,25 @@ namespace Projekt_Pudelko
         {
             return new Pudelko(result.a, result.b, result.c, UnitOfMeasure.milimeter);
         }
+
+        public double this[int index]
+        {
+            get
+            {
+                switch (index) 
+                {
+                    case 0:
+                        return this.a;
+                    case 1:
+                        return this.b;
+                    case 2:
+                        return this.c;
+                    default:
+                        throw new IndexOutOfRangeException();
+                }
+
+            }
+        }
         private double  GetNumberInUnit(double number, string unitAsString)
         {
             UnitOfMeasure unit = UnitOfMeasure.meter;
